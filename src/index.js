5cbf34f6ec2b5cf24bee83657d63b0a3ca91b54e
julioclavijo <julioclavijo@outlook.com>
2024-04-14 21:08:27 -0500

    definición de base mongo

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import app from './app';
 import {Server as socketServer} from 'socket.io';
 import http from 'http';
 
+import { connectDB } from './db';
+
+connectDB();
+
 const server =http.createServer(app);
 const httpServer = server.listen(3000);
 const io = new socketServer(httpServer);
