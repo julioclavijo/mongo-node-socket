617ad9172648dee00c305110050eb5571b0461a6
julioclavijo <julioclavijo@outlook.com>
2024-04-21 15:37:34 -0500

    Configuracion PORT

--- src/index.js
+++ src/index.js
@@ -1,16 +1,17 @@
 // Configuracion del servidor
 import app from './app';
-import {Server as socketServer} from 'socket.io';
+import { Server as socketServer } from 'socket.io';
 import http from 'http';
 import sockets from './sockets';
+import { PORT } from './config';
 
 import { connectDB } from './db';
 
 connectDB();
 
-const server =http.createServer(app);
-const httpServer = server.listen(3000);
-console.log("Server is running on port 3000");
+const server = http.createServer(app);
+const httpServer = server.listen(PORT);
+console.log("Server is running on port ", PORT);
 
 // Se puede validar con: http://localhost:3000/socket.io/socket.io.js
 
